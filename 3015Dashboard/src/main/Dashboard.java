71b7ce8d7882ecaf038cd5f58663093f1ebee4bf
Michael <mjansen4857@gmail.com>
2016-04-26 22:21:29 -0400

    k

--- 3015Dashboard/src/main/Dashboard.java
+++ 3015Dashboard/src/main/Dashboard.java
@@ -1,29 +1,9 @@
 package main;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
-
 public class Dashboard {
-	public static void main(String[] args){
-		new Dashboard().run();
-	}
+	static Frame frame;
 	
-	public void run(){
-		NetworkTable.setClientMode();
-		NetworkTable.setIPAddress("10.30.15.2");
-		NetworkTable table = NetworkTable.getTable("datatable");
-		
-		while(true){
-			try{
-				Thread.sleep(1000);
-			}catch(Exception e){
-				Logger.getLogger(Dashboard.class.getName()).log(Level.SEVERE, null, e);
-			}
-			
-			double test = table.getNumber("test", 3.3);
-			System.out.println("Test: " + test);
-		}
+	public static void main(String[] args){
+		frame = new Frame();
 	}
 }
